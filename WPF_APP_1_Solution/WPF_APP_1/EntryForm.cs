d34ae8679bef6e9d168e8272353db2a1f8e30b50
Sidikiganame-1 <sidiki.ganame-1@ou.edu>
2020-09-18 21:58:38 -0500

    WPF_APP_1_Testing

---
+++ WPF_APP_1_Solution/WPF_APP_1/EntryForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_APP_1
+{
+    class EntryForm
+    {
+        public string Name;
+        public string Address;
+        public int ZipCode;
+
+        public EntryForm()
+        {
+            this.Name = string.Empty;
+            this.Address = string.Empty;
+            this.ZipCode = 555;
+        }
+        public EntryForm(string address, string name, int zipCode)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            string NF = ($"{Name},{Address}, {ZipCode}");
+            return NF;
+        }
+    }
+}
